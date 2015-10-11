5259d2d8f84220acb03f01b2dac6928a1522e93c
Dan Gilkerson <contact@dangilkerson.com>
2015-10-11 12:56:16 -0400

    QR code for debate url

--- client/debate/debate.js
+++ client/debate/debate.js
@@ -1,5 +1,12 @@
 Template.debate.helpers({
-    debate: function() {
+    debate: function () {
         return Debates.findOne({_id: FlowRouter.getParam('debateId')});
+    },
+    url: function () {
+        return window.location.href;
     }
+});
+
+Template.debate.onRendered(function () {
+    this.$('.ui.dropdown').dropdown()
 });
