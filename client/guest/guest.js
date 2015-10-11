858b0105c394b67cc859f8cf1cb31a4db692eb99
Dan Gilkerson <contact@dangilkerson.com>
2015-10-11 06:12:32 -0400

    Define the titles for the two sides

--- client/guest/guest.js
+++ client/guest/guest.js
@@ -1,6 +1,9 @@
 Template.guest.events({
-    'click .debate': function () {
-        var debateId = Debates.start($(".topic").val());
+    'submit form': function (e) {
+        e.preventDefault();
+
+        var data = form2js(e.currentTarget);
+        var debateId = Debates.start(data);
         FlowRouter.go("/" + debateId);
     }
 })
